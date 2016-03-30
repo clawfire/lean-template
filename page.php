99e25d200896f1f0573584e0fed657fb9915ebc1
Thibault Milan <hello@thibaultmilan.com>
2016-03-30 11:35:39 +0200

    Implement the single page basic template

--- page.php
+++ page.php
@@ -8,31 +8,26 @@
  * different template.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package continuous
  */
-
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="ui container">
+		<main role="main">
 
 			<?php
-			while ( have_posts() ) : the_post();
+            while (have_posts()) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+                get_template_part('template-parts/content', 'page');
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) :
+                    comments_template();
+                endif;
 
-			endwhile; // End of the loop.
-			?>
+            endwhile; // End of the loop.
+            ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+<?php //get_sidebar(); ?>
 <?php
-get_sidebar();
 get_footer();
