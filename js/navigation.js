e819d361bc109aca346c297924dd41419c0201a6
Thibault Milan <hello@thibaultmilan.com>
2016-03-31 10:34:35 +0200

    Add the functional JS code for sidebar & header

--- js/navigation.js
+++ js/navigation.js
@@ -4,10 +4,28 @@
  * Handles toggling the navigation menu for small screens and enables TAB key
  * navigation support for dropdown menus.
  */
-( function() {
-    jQuery( document )
+( function( $ ) {
+    $( document )
         .ready( function() {
-            jQuery( '.ui.dropdown' )
+            $( '.ui.dropdown' )
                 .dropdown();
+            // fix menu when passed
+            $( '.masthead' )
+                .visibility( {
+                    once: false,
+                    onBottomPassed: function() {
+                        $( '.fixed.menu' )
+                            .transition( 'fade in' );
+                    },
+                    onBottomPassedReverse: function() {
+                        $( '.fixed.menu' )
+                            .transition( 'fade out' );
+                    }
+                } );
+            // create sidebar and attach to menu open
+            $( '.ui.sidebar' )
+                .sidebar( 'attach events', '.toc.item' );
         } );
-} )();
+
+
+} )( jQuery );
