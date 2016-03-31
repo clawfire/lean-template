eee3ee5cc1ae2cd63cabe6e42999d05945f50af3
Thibault Milan <hello@thibaultmilan.com>
2016-03-31 10:24:15 +0200

    Update the header according our last review

--- header.php
+++ header.php
@@ -18,28 +18,32 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<header id="masthead" class="ui container" role="banner">
-		<nav id="site-navigation" role="navigation" class="ui labeled icon dropdown floated left button" aria-controls="primary-menu" aria-expanded="false">
-			<i class="sidebar icon"></i>
-			<?php esc_html_e('Menu', 'continuous'); ?>
-			<?php wp_nav_menu(array(
-                'theme_location' => 'primary',
-                'menu_id' => 'primary-menu',
-                'container_class' => 'menu',
-                'items_wrap' => '%3$s',
-            )); ?>
+	<!-- Following Menu -->
+	<div class="ui large top fixed hidden menu">
+	<?php wp_nav_menu(array(
+        'theme_location' => 'primary',
+        'menu_id' => 'primary-menu',
+        'container_class' => 'ui container',
+        'items_wrap' => '%3$s',
+    )); ?>
+	</div>
 
-		</nav><!-- #site-navigation -->
-		<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e('Skip to content', 'continuous'); ?></a>
-		<div class="site-branding">
-			<?php
-            if (is_front_page() && is_home()) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-			<?php else : ?>
-				<h2 class="ui center aligned icon header">
-				  <img src="<?= get_header_image() ?>" alt="<?php bloginfo('name')?>" class="ui small image" />
-				  <div class="content">
+
+	<!-- Sidebar Menu -->
+	<?php wp_nav_menu(array(
+        'theme_location' => 'primary',
+        'menu_id' => 'primary-menu',
+        'container_class' => 'ui vertical inverted sidebar menu',
+        'items_wrap' => '%3$s',
+    )); ?>
+
+<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e('Skip to content', 'continuous'); ?></a>
+<div id="page" class="pusher">
+	<?php if (!is_front_page() && !is_home()) : ?>
+		<header id="masthead" class="ui container" role="banner">
+			<h2 class="ui center aligned icon header">
+				<img src="<?= get_header_image() ?>" alt="<?php bloginfo('name')?>" class="ui small image" />
+				<div class="content">
 					<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
 						<?php bloginfo('name');?>
 					</a>
@@ -47,12 +51,8 @@
                     $description = get_bloginfo('description', 'display');
                     if ($description || is_customize_preview()) : ?>
 					<div class="sub header"><?= $description; /* WPCS: xss ok. */ ?></div>
-					<?php endif; ?>
-				  </div>
-				</h2>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+				<?php endif; ?>
+				</div>
+			</h2>
+		</header>
+	<?php endif; ?>
